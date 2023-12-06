4f19ac487e1a0c5a42354cf0fd15be101800af6c
hongjiaherng <jiaherng2001@gmail.com>
2023-12-06 17:32:42 +0800

    Add mmaction2 install scripts

---
+++ feature-extractor/README.md
@@ -0,0 +1,18 @@
+## Starting up in a Kaggle Notebook / Google Colab
+```bash
+!git clone https://github.com/hongjiaherng/inappropriate-video-detection.git
+%cd inappropriate-video-detection/feature-extractor
+!pip install -r requirements.txt
+!./scripts/install_mmaction2.sh
+!./scripts/download_model.sh
+```
+
+## Starting up in your own PC
+```bash
+conda create --name <env-name> python=3.10
+pip install -r requirements.txt
+sh scripts/install_mmaction2.sh
+sh scripts/download_model.sh
+```
+
+
--- feature-extractor/scripts/install_mmaction2.sh
+++ feature-extractor/scripts/install_mmaction2.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+# Install mmaction2
 pip install -U openmim
 mim install mmengine
 mim install mmcv
 pip install mmaction2
 
 # fix the modulenotfounderror in mmaction2
-# %cd /kaggle/working/inappropriate-video-detection/feature-extractor
-# !unzip scripts/drn.zip
-# !ls scripts/drn/
-# !mv scripts/drn/ /opt/conda/lib/python3.10/site-packages/mmaction/models/localizers/
-# !ls /opt/conda/lib/python3.10/site-packages/mmaction/models/localizers
+
+# Get the location of the mmaction2 package
+package_location=$(pip show mmaction2 | grep -i "Location" | awk -F 'Location: ' 'NF>1{print $2}' | tr -d '\r')
+
+# Specify the subdirectory to append to the package location
+subdirectory="mmaction/models/localizers"
+
+# Create the target directory by appending the subdirectory
+target_directory="$package_location/$subdirectory"
+
+unzip "scripts/drn.zip"
+
+mv "drn" "$target_directory"
+
+echo "Moved 'drn' folder into '$target_directory'."

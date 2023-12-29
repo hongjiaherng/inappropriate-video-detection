0e8e0a989f3e8efdade1d7610822338dda7d6734
hongjiaherng <jiaherng2001@gmail.com>
2023-12-29 23:05:15 +0800

    Append C3D to model installation script

--- feature-extractor/scripts/download_models.sh
+++ feature-extractor/scripts/download_models.sh
@@ -5,9 +5,12 @@ target_directory="pretrained/"
 # Create the directory if it doesn't exist
 mkdir -p "$target_directory"
 
-# Download I3D, swin
+# Download I3D, swin, C3D
 curl -O https://download.openmmlab.com/mmaction/v1.0/recognition/i3d/i3d_imagenet-pretrained-r50-nl-dot-product_8xb8-32x2x1-100e_kinetics400-rgb/i3d_imagenet-pretrained-r50-nl-dot-product_8xb8-32x2x1-100e_kinetics400-rgb_20220812-8e1f2148.pth
 mv i3d_imagenet-pretrained-r50-nl-dot-product_8xb8-32x2x1-100e_kinetics400-rgb_20220812-8e1f2148.pth "$target_directory"
 
 curl -O https://download.openmmlab.com/mmaction/v1.0/recognition/swin/swin-tiny-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb/swin-tiny-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb_20220930-241016b2.pth
 mv swin-tiny-p244-w877_in1k-pre_8xb8-amp-32x2x1-30e_kinetics400-rgb_20220930-241016b2.pth "$target_directory"
+
+curl -O https://download.openmmlab.com/mmaction/v1.0/recognition/c3d/c3d_sports1m-pretrained_8xb30-16x1x1-45e_ucf101-rgb/c3d_sports1m-pretrained_8xb30-16x1x1-45e_ucf101-rgb_20220811-31723200.pth
+mv c3d_sports1m-pretrained_8xb30-16x1x1-45e_ucf101-rgb_20220811-31723200.pth "$target_directory"
